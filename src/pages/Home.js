3f5e59a0c6aa09c9888a7fddb10debb026edda52
Rafael Roman Garcia <rafael.roman@clipdashboard.com>
2022-04-20 16:31:49 -0500

    update user context provider

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,25 +3,26 @@ import { SpotifyContainer } from '../Helpers/styled';
 import Body from '../Components/Body/Body';
 import SideBar from '../Components/SideBar/SideBar';
 import Footer from '../Components/Footer/Footer';
-import { UserContext } from "../Provider/userProvider";
+import { useUser } from "../Provider/userProvider";
 
 const Home = () => {
   // const user = useContext(NameUserContext)
+  const { loginData, logout } = useUser();
 
   const handleLogout = () => {
     localStorage.removeItem('loginData');
-    // setLoginData(null); 
+    // setLoginData(null);
   }
 
   return (
-      <UserContext.Consumer>
+    <>
         <SpotifyContainer>
             <SideBar />
-            {value => <h1>{value.name}</h1>}
+              <h1>{loginData.name}</h1>
             <Body />
         </SpotifyContainer>
-        <Footer />
-    </UserContext.Consumer>
+        <Footer />\
+    </>
   );
 }
 
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,52 +3,52 @@ import GoogleLogin from 'react-google-login';
 import Home from './Home';
 import imgSpotify from '../Helpers/icons/Spotify_Logo_Black.png'
 import { Root, LoginContainer } from '../Helpers/styled';
+import { useUser } from '../Provider/userProvider';
 
-export const UserContext = createContext();
+// export const UserContext = createContext();
 
 const Login = () => {
-const [loginData, setLoginData] = useState(
-    localStorage.getItem('loginData')
-      ? JSON.parse(localStorage.getItem('loginData'))
-      : null
-  );
+  const { login, logout, loginData } = useUser();
+// const [loginData, setLoginData] = useState(
+//     localStorage.getItem('loginData')
+//       ? JSON.parse(localStorage.getItem('loginData'))
+//       : null
+//   );
 
   const handleFailure = (result) => {
     alert(result);
   };
-  const handleLogin = async (googleData) => {
-    const res = await fetch('/api/google-login', {
-      method: 'POST', 
-      body: JSON.stringify({
-        token: googleData.tokenId,
-      }),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
+  // const handleLogin = async (googleData) => {
+  //   const res = await fetch('/a+pi/google-login', {
+  //     method: 'POST', 
+  //     body: JSON.stringify({
+  //       token: googleData.tokenId,
+  //     }),
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //     },
+  //   });
 
-    const data = await res.json();
-    setLoginData(data);
-    localStorage.setItem('loginData', JSON.stringify(data));
-  };
-  const handleLogout = () => {
-    localStorage.removeItem('loginData');
-    setLoginData(null);
-  }
+  //   const data = await res.json();
+  //   setLoginData(data);
+  //   localStorage.setItem('loginData', JSON.stringify(data));
+  // };
+  // const handleLogout = () => {
+  //   localStorage.removeItem('loginData');
+  //   setLoginData(null);
+  // }
   return(
     <Root>
         <div>
          {loginData ? (
-           <UserContext.Provider value={loginData}>
               <Home user={loginData}/>
-            </UserContext.Provider>
           ) : (
             <LoginContainer>
               <img src={imgSpotify} alt="logo" />
               <GoogleLogin
                 clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
                 buttonText="Log in with Google"
-                onSuccess={handleLogin}
+                onSuccess={login}
                 onFailure={handleFailure}
                 cookiePolicy={'single_host_origin'}
                 data-testid="login"
