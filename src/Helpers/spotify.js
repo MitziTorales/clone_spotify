b7443e066098a6c81d955769c92708107d1072e6
MitziTorales <mitzi.torales.04@gmail.com>
2022-05-02 16:56:03 -0500

    Add icons and add scopes

--- src/Helpers/spotify.js
+++ src/Helpers/spotify.js
@@ -2,11 +2,23 @@ export const authEndpoint = "https://accounts.spotify.com/authorize";
 const clientId = '3ed3b11c86b4444399c6be191dc7dd2f';
 const redirectUri = "http://localhost:3000/";
 const scopes = [
-  "user-read-currently-playing",
-  "user-read-recently-played",
-  "user-read-playback-state",
-  "user-top-read",
+  "user-read-playback-position",
+  "user-read-email",
+  "user-library-modify",
+  "playlist-modify-public",
+  "ugc-image-upload",
+  "user-follow-modify",
   "user-modify-playback-state",
+  "user-read-recently-played",
+  "user-read-private",
+  "user-library-read",
+  "user-top-read",
+  "playlist-modify-private",
+  "user-follow-read",
+  "user-read-playback-state",
+  "user-read-currently-playing",
+  "playlist-read-private",
+  "playlist-read-collaborative",
 ];
 
 export const getTokenFromResponse = () => {
